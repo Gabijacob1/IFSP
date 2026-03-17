60224d84f445037b388e48cda6eae56c0a7f192e
Gabijacob1 <gabribela.tj@icloud.com>
2026-03-17 15:54:25 -0300

    salvando

--- Algoritmos e Programação de computadores/ex1.2.c
+++ Algoritmos e Programação de computadores/ex1.2.c
@@ -1,17 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 int main(void) {
-    int n1=4;
-    for(int i=0; i<=1; i++){
-        for(int j=1; j<=n1-1; j++){
-            printf(" ");
-        }
-        for(int k=1; k<=i+1; k++){
-            printf("*");
-        }
-        printf("\n");
-
-    }
+    printf("   *\n");
+    printf("  ***\n");
+    printf(" *****\n");
+    printf("*******\n");
     return 0;
 }
