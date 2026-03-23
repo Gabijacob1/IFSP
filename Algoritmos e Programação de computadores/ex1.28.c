d09f21739df33cad70363731bc41ac8c6438e235
Gabijacob1 <gabribela.tj@icloud.com>
2026-03-23 16:56:30 -0300

    salvando

---
+++ Algoritmos e Programação de computadores/ex1.28.c
@@ -0,0 +1,15 @@
+# include <stdio.h>
+# include <stdlib.h>
+
+int main(void) {
+    int n1;
+    printf("Quantidade de segundos: ");
+    scanf("%d", &n1);
+    int horas = n1 / 3600;
+    int minutos = (n1 % 3600) / 60;
+    int segundos = n1 % 60;
+    printf("%d segundos correspondem a %d hora(s), %d minuto(s) e %d segundo(s)\n",
+         n1, horas, minutos, segundos);
+
+    return 0;
+}
