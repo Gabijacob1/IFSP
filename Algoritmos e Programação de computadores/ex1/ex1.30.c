a9a7768fe3b7c718ff3f92577a7bc2b0769c08cf
Gabijacob1 <gabribela.tj@icloud.com>
2026-03-23 17:26:22 -0300

    salvando

--- Algoritmos e Programação de computadores/ex1/ex1.30.c
+++ Algoritmos e Programação de computadores/ex1/ex1.30.c
@@ -1,13 +1,13 @@
 # include <stdio.h>
 # include <stdlib.h>
 int main(void) {
-    int n1, n2, n3, n4;
+    int n1, n2;
     printf("Digite o primeiro numero: ");
     scanf("%d", &n1);
     printf("Digite o segundo numero: ");
     scanf("%d", &n2);
-    n3 = n1;
-    n4 = n2;
+    int n3 = n1;
+    int n4 = n2;
     printf("Valores antes da troca: \n");
     printf("Primeiro numero: %d \n", n1);
     printf("Segundo numero: %d \n", n2);
