8961a49e499b9f631cfcd0a2820163e826bb5a07
Gabijacob1 <gabribela.tj@icloud.com>
2026-03-24 16:48:31 -0300

    salvando

--- Algoritmos e Programação de computadores/ex2/exercicios/ex1.3.c
+++ Algoritmos e Programação de computadores/ex2/exercicios/ex1.3.c
@@ -5,7 +5,7 @@
 int main( void ) {
    
     
-    int x1, y1, x2, y2;
+    int x1, y1, x2, y2, altura;
         printf( "Digite as coordenadas x inicial: " );
         scanf( "%d", &x1 );
         printf( "Digite as coordenadas y inicial: " );
@@ -14,18 +14,26 @@ int main( void ) {
         scanf( "%d", &x2 );
         printf( "Digite as coordenadas y final: " );
         scanf( "%d", &y2 );
+        printf( "Digite a altura: " );
+        scanf( "%d", &altura );
         
-    InitWindow( 600, 400, "Exercício Criativo 1.2" );
+        int xtopo = (x1 + x2) / 2;
+        int ytopo = y1 - altura;
+
+    InitWindow( 600, 400, "Exercício Criativo 1.3" );
 
     SetTargetFPS( 60 );    
 
     while ( !WindowShouldClose() ) {
+        
 
         BeginDrawing();
 
         ClearBackground( BLACK );
        
-        DrawLine( x1, y1, x2, y2, RED );
+        DrawLine( x1, y1, x2, y2, BLUE );
+        DrawLine( x1, y1, xtopo, ytopo, YELLOW );
+        DrawLine( x2, y2, xtopo, ytopo, RED );
 
 
         EndDrawing();
