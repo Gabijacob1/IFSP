c1fcb6375ac50685fcb801a1b9aef3a626bb1150
Gabijacob1 <gabribela.tj@icloud.com>
2026-03-24 17:10:43 -0300

    salvando

---
+++ Algoritmos e Programação de computadores/ex2/exercicios/ex1.7.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <raylib.h>
+int main( void ) {
+  
+    int posX, posY, raio;
+
+        printf( "x: " );
+        scanf( "%d", &posX );
+        printf( "y: " );
+        scanf( "%d", &posY );
+        printf( "raio: " );
+        scanf( "%d", &raio );
+       
+    SetConfigFlags( FLAG_MSAA_4X_HINT );
+
+    InitWindow( 600, 400, "Exercício Criativo 1.6" );
+
+    SetTargetFPS( 60 );    
+
+    while ( !WindowShouldClose() ) {
+
+        BeginDrawing();
+
+        ClearBackground( BLACK );
+       
+         DrawCircle( posX, posY, raio, PURPLE );    
+
+        EndDrawing();
+    }
+    // fecha a janela
+    CloseWindow();
+    return 0;
+
+}
