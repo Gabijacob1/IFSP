abf2e1f9acdf69291f325445085d2e0c80c904bf
Gabijacob1 <gabribela.tj@icloud.com>
2026-03-24 15:58:15 -0300

    salvando

---
+++ Algoritmos e Programação de computadores/ex2/exercicios/ex1.1.c
@@ -0,0 +1,63 @@
+/**
+ * @file main.c
+ * @author Prof. Dr. David Buzatto
+ * @brief Modelo para desenvolvimento de exercícios criativos usando a engine
+ * de jogos Raylib (https://www.raylib.com/).
+ * 
+ * @copyright Copyright (c) 2025
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#include <raylib.h>
+
+int main( void ) {
+   
+    
+    int x, y;
+        printf( "Digite as coordenadas x: " );
+        scanf( "%d", &x );
+        printf( "Digite as coordenadas y: " );
+        scanf( "%d", &y );
+        
+       
+    // ativa a suavização (antialiasing)
+    //SetConfigFlags( FLAG_MSAA_4X_HINT );
+
+    // cria uma janela de 800 pixels de largura por 600 de altura
+    InitWindow( 600, 400, "Exercício Criativo 1.1" );
+
+    // configura a quantidade de quatros por segundo da engine
+    SetTargetFPS( 60 );    
+
+    // enquanto não é sinalizado que a janela deve ser fechada
+    while ( !WindowShouldClose() ) {
+
+        // inicia o processo de desenho
+        BeginDrawing();
+
+        // limpa a tela usando uma cor
+        ClearBackground( BLACK );
+
+        /*----------------------------------------------------------------------
+         * A lógica do seu desenho deve vir aqui.
+         ---------------------------------------------------------------------*/
+        DrawPixel( x, y, RED );
+        
+        
+        /*----------------------------------------------------------------------
+         * A lógica do seu desenho deve terminar na linha acima.
+         ---------------------------------------------------------------------*/
+
+        // termina o desenho
+        EndDrawing();
+
+    }
+
+    // fecha a janela
+    CloseWindow();
+    return 0;
+
+}
