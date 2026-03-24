cdd046a83107d37ded61010a997ff8296a724c55
Gabijacob1 <gabribela.tj@icloud.com>
2026-03-24 16:17:34 -0300

    salvando

---
+++ Algoritmos e Programação de computadores/ex2/exercicios/ex1.2.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <raylib.h>
+int main( void ) {
+   
+    
+    int x1, y1, x2, y2;
+        printf( "Digite as coordenadas x inicial: " );
+        scanf( "%d", &x1 );
+        printf( "Digite as coordenadas y inicial: " );
+        scanf( "%d", &y1 );
+        printf( "Digite as coordenadas x final: " );
+        scanf( "%d", &x2 );
+        printf( "Digite as coordenadas y final: " );
+        scanf( "%d", &y2 );
+        
+    InitWindow( 600, 400, "Exercício Criativo 1.1" );
+
+    SetTargetFPS( 60 );    
+
+    while ( !WindowShouldClose() ) {
+
+        BeginDrawing();
+
+        ClearBackground( BLACK );
+       
+        DrawLine( x1, y1, x2, y2, RED );
+
+
+        EndDrawing();
+    }
+    // fecha a janela
+    CloseWindow();
+    return 0;
+
+}
