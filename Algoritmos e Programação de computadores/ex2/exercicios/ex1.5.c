abc2229b8fcec3f3b024ad36a07ed9d3a96744ff
Gabijacob1 <gabribela.tj@icloud.com>
2026-03-24 16:57:52 -0300

    salvando

---
+++ Algoritmos e Programação de computadores/ex2/exercicios/ex1.5.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <raylib.h>
+int main( void ) {
+  
+    int posX, posY, width, height;
+
+        printf( "x: " );
+        scanf( "%d", &posX );
+        printf( "y: " );
+        scanf( "%d", &posY );
+        printf( "largura: " );
+        scanf( "%d", &width );
+        printf( "altura: " );
+        scanf( "%d", &height );
+       
+
+    InitWindow( 600, 400, "Exercício Criativo 1.4" );
+
+    SetTargetFPS( 60 );    
+
+    while ( !WindowShouldClose() ) {
+
+        BeginDrawing();
+
+        ClearBackground( BLACK );
+       
+       DrawRectangle( posX, posY, width, height, GREEN );
+
+
+        EndDrawing();
+    }
+    // fecha a janela
+    CloseWindow();
+    return 0;
+
+}
