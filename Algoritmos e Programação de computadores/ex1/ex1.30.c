a0db5fc3ab11afcd3faa138114505d1e606201c8
Gabijacob1 <gabribela.tj@icloud.com>
2026-03-26 15:10:15 -0300

    salvando

--- Algoritmos e Programação de computadores/ex1/ex1.30.c
+++ Algoritmos e Programação de computadores/ex1/ex1.30.c
@@ -8,12 +8,13 @@ int main(void) {
     scanf("%d", &n2);
     int n3 = n1;
     int n4 = n2;
-    printf("Valores antes da troca: \n");
-    printf("Primeiro numero: %d \n", n1);
-    printf("Segundo numero: %d \n", n2);
-    printf("Valores depois da troca: \n");
-    printf("Primeiro numero: %d \n", n4);
-    printf("Segundo numero: %d \n", n3);
+    printf("Valores antes da troca:\n");
+    printf("Primeiro numero: %d\n", n1);
+    printf("Segundo numero: %d\n", n2);
+
+    printf("Valores depois da troca:\n");
+    printf("Primeiro numero: %d\n", n4);
+    printf("Segundo numero: %d\n", n3);
 
     return 0;
 }
