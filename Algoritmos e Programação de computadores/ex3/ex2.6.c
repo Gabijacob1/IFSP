123c509e0957c393fde90cb179488fb60832e273
Gabijacob1 <gabribela.tj@icloud.com>
2026-03-30 16:54:37 -0300

    salvando

--- Algoritmos e Programação de computadores/ex3/ex2.6.c
+++ Algoritmos e Programação de computadores/ex3/ex2.6.c
@@ -1,7 +1,17 @@
 # include <stdio.h>
 # include <stdlib.h>
 int main(void) {
-    float n1;
+    float n1, n2;
+    printf("Digite um numero: ");
+    scanf("%f", &n1);
+    if (n1>=20){
+        n2 = n1/2;
+        printf("A metade de %.2f e %.2f\n", n1, n2);
+    }
+    else{
+        n2 = n1*3;
+        printf("O triplo de %.2f e %.2f\n", n1, n2);
+    }
     
 
     return 0;
