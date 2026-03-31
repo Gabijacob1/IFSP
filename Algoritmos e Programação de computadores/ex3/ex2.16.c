1fe7e910a0f3b57f5f016b73c7957bb3f8a3cd40
Gabijacob1 <gabribela.tj@icloud.com>
2026-03-31 17:41:59 -0300

    salvando

--- Algoritmos e Programação de computadores/ex3/ex2.16.c
+++ Algoritmos e Programação de computadores/ex3/ex2.16.c
@@ -47,26 +47,45 @@ int main(void) {
             printf("CM");
         }
 
-        if (dezena == 1) printf("X");
-        else if (dezena == 2) printf("XX");
-        else if (dezena == 3) printf("XXX");
-        else if (dezena == 4) printf("XL");
-        else if (dezena == 5) printf("L");
-        else if (dezena == 6) printf("LX");
-        else if (dezena == 7) printf("LXX");
-        else if (dezena == 8) printf("LXXX");
-        else if (dezena == 9) printf("XC");
+        if (dezena == 1){ 
+            printf("X");
+        } else if (dezena == 2) {
+            printf("XX");
+        } else if (dezena == 3) {
+            printf("XXX");
+        } else if (dezena == 4) {
+            printf("XL");
+        } else if (dezena == 5) {
+            printf("L");
+        } else if (dezena == 6) {
+            printf("LX");
+        } else if (dezena == 7) {
+            printf("LXX");
+        } else if (dezena == 8) {
+            printf("LXXX");
+        } else if (dezena == 9) {
+            printf("XC");
+        }
 
-        
-        if (unidade == 1) printf("I");
-        else if (unidade == 2) printf("II");
-        else if (unidade == 3) printf("III");
-        else if (unidade == 4) printf("IV");
-        else if (unidade == 5) printf("V");
-        else if (unidade == 6) printf("VI");
-        else if (unidade == 7) printf("VII");
-        else if (unidade == 8) printf("VIII");
-        else if (unidade == 9) printf("IX");
+        if (unidade == 1) {
+            printf("I");
+        } else if (unidade == 2) {
+            printf("II");
+        } else if (unidade == 3) {
+            printf("III");
+        } else if (unidade == 4) {
+            printf("IV");
+        } else if (unidade == 5) {
+            printf("V");
+        } else if (unidade == 6) {
+            printf("VI");
+        } else if (unidade == 7) {
+            printf("VII");
+        } else if (unidade == 8) {
+            printf("VIII");
+        } else if (unidade == 9) {
+            printf("IX");
+        }
     }
 
     return 0;
