1c6ed71f030f3063ccce87d218ccdc9d2f6a13df
Gabijacob1 <gabribela.tj@icloud.com>
2026-04-07 16:41:46 -0300

    salvando

--- Algoritmos e Programação de computadores/ex3/ex3.9.c
+++ Algoritmos e Programação de computadores/ex3/ex3.9.c
@@ -2,10 +2,12 @@
 #include <stdlib.h>
 int main(void) {
     int n1;
-    printf("Digite um numero inteiro: ");
+    int n2;
+    printf("Tabuada do Numero: ");
     scanf("%d", &n1);
-    for (int i = 1; i <= n1; i++){
-       printf("%d ", i);
+    for (int i = 0; i <= 10; i++){
+        n2 = n1 * i;
+        printf("%d x %d = %d\n", n1, i, n2);
     }
 
     return 0;
