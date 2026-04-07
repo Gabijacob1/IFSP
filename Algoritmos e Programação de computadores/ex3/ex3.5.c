97f9a8f614557866a4d9304eb4caf2874977e61a
Gabijacob1 <gabribela.tj@icloud.com>
2026-04-07 15:32:30 -0300

    salvando

--- Algoritmos e Programação de computadores/ex3/ex3.5.c
+++ Algoritmos e Programação de computadores/ex3/ex3.5.c
@@ -4,11 +4,11 @@ int main(void) {
     int n1;
     printf("Forneca um numero maior ou igual a zero: ");
     scanf("%d", &n1);
-
-    for (int i = 0; i <= n1; i++){
-       printf("%d ", i);
-    }
-    if (n1 < 0){
+    if (n1 >= 0){
+        for (int i = 0; i <= n1; i++){
+        printf("%d ", i);
+        }
+    }else{
         printf("Valor incorreto (negativo)");
     }
 
