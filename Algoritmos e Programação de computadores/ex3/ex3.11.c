9f242005448ddf2930a062bfd9e7085935fd26a3
Gabijacob1 <gabribela.tj@icloud.com>
2026-04-07 17:02:01 -0300

    salvando

---
+++ Algoritmos e Programação de computadores/ex3/ex3.11.c
@@ -0,0 +1,24 @@
+# include <stdio.h>
+# include <stdlib.h>
+int main(void) {
+    int n1;
+    int n2;
+
+    printf("N1: ");
+    scanf("%d", &n1);
+
+    printf("N2: ");
+    scanf("%d", &n2);
+   
+    if (n2 > n1){
+        for (int i = n1; i <= n2; i++){
+        printf("%d ", i);
+        }
+    } else if (n2 < n1){
+        for (int i = n1; i >= n2; i--){
+        printf("%d ", i);
+        }
+    } else {
+        printf("Os numeros fornecidos sao iguais!");
+    }
+}
