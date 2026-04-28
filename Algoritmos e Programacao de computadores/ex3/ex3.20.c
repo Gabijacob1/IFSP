48e823c02a377b006df6771c04cd66dbbf3add3b
Gabijacob1 <gabribela.tj@icloud.com>
2026-04-28 11:02:12 -0300

    salvando

--- Algoritmos e Programacao de computadores/ex3/ex3.20.c
+++ Algoritmos e Programacao de computadores/ex3/ex3.20.c
@@ -1,50 +1,47 @@
-# include <stdio.h>
-# include <stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
+
 int main(void) {
-    
+
     for (int i = 0; i < 5; i++){
         for (int j = 0; j <= i; j++){
-                printf("*");
-            
+            printf("*");
         }
         printf("\n");
     }
 
-        printf("\n");
+    printf("\n");
 
     for (int i = 0; i < 5; i++){
         for (int j = i; j <= 4; j++){
-                printf("*");
-            
-        }
-        printf("\n");
-    }
-        printf("\n");
-
-    for (int i = 0; i <= 5; i++){
-        for (int j = 5; j > i; j--){
-                printf(" ");      
-        }
-        for (int j = 0; j < i; j++){
             printf("*");
         }
         printf("\n");
     }
+
     printf("\n");
 
-    for (int y = 0; y <= 4; y++){
-        for (int x = 1; x <= y; x++){
+    for (int i = 0; i < 5; i++){
+        for (int j = 0; j < 4 - i; j++){
             printf(" ");
         }
-        for (int x = 4; x >= y; x--){
+        for (int j = 0; j <= i; j++){
             printf("*");
         }
-        
         printf("\n");
     }
-           printf("\n");
+
+    printf("\n");
+
+    for (int y = 0; y < 5; y++){
+        for (int x = 0; x < y; x++){
+            printf(" ");
+        }
+        for (int x = 0; x < 5 - y; x++){
+            printf("*");
+        }
+        printf("\n");
+    }
 
     return 0;
-
-
 }
