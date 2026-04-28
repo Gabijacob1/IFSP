e3b35e8bad14b23161895729e450ada7d145aa2b
Gabijacob1 <gabribela.tj@icloud.com>
2026-04-28 11:32:40 -0300

    salvando

--- Algoritmos e Programacao de computadores/ex3/ex3.29.c
+++ Algoritmos e Programacao de computadores/ex3/ex3.29.c
@@ -1,9 +1,7 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 int main(void){
-    float n1;
-    float n3;
+    float n1, n3;
     int n2;
 
     printf("Saldo inicial: ");
@@ -12,34 +10,37 @@ int main(void){
     printf("Operacoes: \n");
     printf("1) Deposito; \n2) Saque; \n3) Fim.\n");
 
-    do{
-        printf("\nOperacao desenada: ");
+    do {
+        printf("\nOperacao desejada: ");
         scanf("%d", &n2);
 
-        if (n2 == 1){
-            printf("\nValor a depositar: ");
+        if(n2 == 1){
+            printf("Valor a depositar: ");
             scanf("%f", &n3);
             n1 += n3;
-        } else if (n2 == 2){
-            printf("\nValor a sacar: ");
+        }
+        else if(n2 == 2){
+            printf("Valor a sacar: ");
             scanf("%f", &n3);
             n1 -= n3;
-        }else if (n2 != 3){
-            printf("\nOperacao invalida.\n");
+        }
+        else if(n2 != 3){
+            printf("Operacao invalida.\n");
         }
 
     } while(n2 != 3);
-               
-        printf("Saldo final: %.2f\n", n1);
-    
-        
-        if(n1 > 0){
-            printf("Conta preferencial.\n");
-        }else if(n1 < 0){
-            printf("Conta devedora.\n");
-        } else{
-            printf("Sem Saldo.\n");
-        }
+
+    printf("Saldo final: R$%.2f\n", n1);
+
+    if(n1 > 0){
+        printf("Conta preferencial.\n");
+    }
+    else if(n1 < 0){
+        printf("Conta devedora.\n");
+    }
+    else{
+        printf("Sem saldo.\n");
+    }
 
     return 0;
 }
