41fb818cbbf829de4e0be5574cceb27a1cba434f
Gabijacob1 <gabribela.tj@icloud.com>
2026-04-28 09:57:32 -0300

    salvando

---
+++ Algoritmos e Programacao de computadores/ex3/ex3.29.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void){
+    float n1;
+    float n3;
+    int n2;
+
+    printf("Saldo inicial: ");
+    scanf("%f", &n1);
+
+    printf("Operacoes: \n");
+    printf("1) Deposito; \n2) Saque; \n3) Fim.\n");
+
+    do{
+        printf("Operacao desenada: ");
+        scanf("%d", &n2);
+
+        if (n2 == 1){
+            printf("Valor a depositar: ");
+            scanf("%f", &n3);
+            n1 += n3;
+        }
+
+        if (n2 == 2){
+            printf("Valor a sacar: ");
+            scanf("%f", &n3);
+            n1 -= n3;
+        }
+
+    } while(n2 == 1 || n2 == 2);
+
+        if(n1 == 0 && n2 == 3){
+            printf("Saldo final: %.2f\n", n1);
+            printf("Sem Saldo.");
+        }
+    
+        if(n1 <= -1){
+            printf("Saldo final: %.2f\n", n1);
+            printf("Conta devedora.");
+        }
+
+        if(n1 > 0){
+            printf("Saldo final: %.2f\n", n1);
+            printf("Conta preferencial.");
+        }
+
+        if(n2 != 1 && n2 != 2 && n2 != 3){
+            printf("Saldo final: %.2f\n", n1);
+            printf("Operacao invalida.");
+        }
+
+    return 0;
+}
