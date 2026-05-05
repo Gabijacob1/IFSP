e40d30d11a12122dfd7c8039bf902fecfd19e92b
Gabijacob1 <gabribela.tj@icloud.com>
2026-05-05 16:48:49 -0300

    salvando

---
+++ Algoritmos e Programacao de computadores/ex4/ex4.6.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main() {
+    int array[5];
+    int n1;
+    int q = 0;
+    int result[q];
+
+    for (int i = 0; i < 5; i++) {
+        printf("array[%d]: ", i);
+        scanf("%d", &array[i]);
+    }
+
+    printf("Buscar por: ");
+    scanf("%d", &n1);
+
+    for (int i = 0; i < 5; i++) {
+        if(n1 == array[i]) {
+            result[q] = i;
+            q++;
+        }
+    }
+    if( q == 0) {
+        printf("O array nao contem o valor %d.\n", n1);
+    } else if (q == 1) {
+        printf("O valor %d foi encontrado no indice %d do array.\n",n1, result[0]);
+    } else {
+        printf("O valor %d foi encontrado nos indices ", n1);
+        for (int i = 0; i < q; i++) {
+            printf("%d, ", result[i]);
+        }
+        printf("do array.\n");
+    }
+
+    return 0;
+}
