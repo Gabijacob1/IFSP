e5b1f482768ee4f08da80dcc9a6d67140f17c83e
Gabijacob1 <gabribela.tj@icloud.com>
2026-05-05 17:36:00 -0300

    salvando

---
+++ Algoritmos e Programacao de computadores/ex4/ex4.7.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int array1[5];
+    int array2[5];
+    int array3[5];
+
+    printf("Forneca os valores do primeiro array:\n");
+    for (int i = 0; i < 5; i++) {
+        printf("array1[%d]: ", i);
+        scanf("%d", &array1[i]);
+    }
+    printf("\n");
+
+    printf("Forneca os valores do segundo array:\n");
+    for (int i = 0; i < 5; i++) {
+        printf("array2[%d]: ", i);
+        scanf("%d", &array2[i]);
+    }
+
+    for (int i = 0; i < 5; i++) {
+        array3[i] = array1[i] + array2[i];
+    }
+
+    for (int i = 0; i < 5; i++) {
+        printf("arraySoma[%d]: %d\n", i, array3[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
+
+    
---
+++ Algoritmos e Programacao de computadores/ex4/ex4.8.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+/*Escreva um programa que preencha um array de números inteiros de 5 posições com valores
+fornecidos pelo usuário. O programa deve exibir os números pares desse array e depois os números
+ímpares, todos na ordem em que aparecem no array*/
+
+int main() {
+    int array[5];
+    int q = 0;
+    int n1[5];
+    int t = 0;
+    int n2[5];
+
+    for (int i = 0; i < 5; i++) {
+        printf("array[%d]: ", i);
+        scanf("%d", &array[i]);
+    }
+
+    for(int i = 0; i < 5; i++) {
+        if (array[i] % 2 == 0) {
+            n1[q] = array[i];
+            q++;
+        }else {
+            n2[t] = array[i];
+            t++;
+        }
+    }
+    
+    printf("Numeros pares: ");
+
+    if (q > 0) {
+        for (int i = 0; i < q; i++) {
+            printf("%d ", n1[i]);
+        }
+        printf("\n");
+    } else {
+        printf("nao ha.\n");
+    }
+
+    printf("Numeros impares: ");
+    
+    if (t > 0) {
+        for (int i = 0; i < t; i++) {
+            printf("%d ", n2[i]);
+        }
+        printf("\n");
+    } else {
+        printf("nao ha.\n");
+    }
+
+
+
+    return 0;
+}
