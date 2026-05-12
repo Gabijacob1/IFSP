b4edf9b9fd60c0f0e6f8259f94db8e86431af9c2
Gabijacob1 <gabribela.tj@icloud.com>
2026-05-12 15:52:16 -0300

    salvando

---
+++ Algoritmos e Programacao de computadores/ex4/ex4.13.c
@@ -0,0 +1,34 @@
+# include <stdio.h>
+# include <stdlib.h>
+
+int main(void) {
+    int array[10];
+    int n1;
+
+    for (int i = 0; i < 10; i++){
+        printf("array[%d]: ", i);
+        scanf("%d", &array[i]);
+    }
+
+    do {
+            printf("Quantidade de elementos (1 a 9): ");
+            scanf("%d", &n1);
+            if (n1 < 1 || n1 > 9) {
+                printf("Quantidade incorreta, forneca novamente!\n");
+            }
+    }while(n1 < 1 || n1 > 9);
+
+    for(int i = n1; i < 10; i++){
+        array[i] = array[i+1];
+    }
+
+    printf("\n");
+
+    for (int i = 0; i < 9; i++){
+        printf("array[%d]: %d\n", i, array[i]);
+    }
+
+
+
+    return 0;
+}
