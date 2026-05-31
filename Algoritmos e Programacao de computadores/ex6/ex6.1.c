7cc491c67a2a113726ae95e02062a2cac1d70cad
Gabijacob1 <gabribela.tj@icloud.com>
2026-05-31 19:32:45 -0300

    salvando

--- Algoritmos e Programacao de computadores/ex6/ex6.1.c
+++ Algoritmos e Programacao de computadores/ex6/ex6.1.c
@@ -21,6 +21,8 @@ int main(void){
        printf("Nao existe equacao de segundo grau!");
     }else {
         delta = b * b - 4 * a * c;
+        printf("Delta: %.2f \n", delta);
+
         if (delta < 0) {
             printf("Delta: %.2f", delta);
             printf("S = {}");
