6a1049ef4b15a5089f450016f5987efbe857f2c5
BV3071804 <gabribela.tj@icloud.com>
2026-08-11 11:35:08 -0300

    salvando

---
+++ Programacao-Orientada-a-Objetos/HackerRank/StdinStdout1.java
@@ -0,0 +1,24 @@
+import java.util. *;
+
+public class StdinStdout1.java {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n1 = scan.nextInt();
+        int n2 = scan.nextInt();
+        int n3 = scan.nextInt();
+
+        System.out.println(n1);
+        System.out.println(n2);
+        System.out.println(n3);
+        scanner.close();
+
+
+
+
+
+        System.out.println(a);
+
+    }
+}
+}
